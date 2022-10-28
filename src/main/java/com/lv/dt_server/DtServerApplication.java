1a01982eff55b7725a6ec7834674dfa69b934d0b
lv <lv@lv.com>
2022-10-28 23:03:23 +0800

    init

---
+++ src/main/java/com/lv/dt_server/DtServerApplication.java
@@ -0,0 +1,13 @@
+package com.lv.dt_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lv/dt_server/DtServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lv.dt_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
